2322060c2d890d73cf2bd937f1414ceb6a185956
MrGrzesiek <78262537+MrGrzesiek@users.noreply.github.com>
2023-12-12 16:21:47 +0100

    database create

---
+++ database/db/init.sql
@@ -0,0 +1,70 @@
+CREATE TABLE IF NOT EXISTS `parking_rates`
+(
+ `id`                        int NOT NULL ,
+ `hour_rate`                 decimal NOT NULL ,
+ `free_entry_minutes`        int NOT NULL ,
+ `exit_grace_period_minutes` int NOT NULL ,
+
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `spot`
+(
+ `id`             int NOT NULL ,
+ `status`         int NOT NULL ,
+ `reg_number`     varchar(20) NOT NULL ,
+ `entry_time`     datetime NOT NULL ,
+ `departure_time` datetime NOT NULL ,
+
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `spot_history`
+(
+ `id`             int NOT NULL ,
+ `entry_time`     datetime NOT NULL ,
+ `departure_time` datetime NOT NULL ,
+ `reg_number`     varchar(20) NOT NULL ,
+ `spot_id`        int NOT NULL ,
+
+PRIMARY KEY (`id`),
+KEY `FK_1` (`spot_id`),
+CONSTRAINT `FK_3` FOREIGN KEY `FK_1` (`spot_id`) REFERENCES `spot` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `entry`
+(
+ `id`         int NOT NULL ,
+ `reg_number` varchar(20) NOT NULL ,
+ `entry_time` datetime NOT NULL ,
+ `photo`      varchar(100) NOT NULL ,
+
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `payment`
+(
+ `id`       int NOT NULL ,
+ `data`     datetime NOT NULL ,
+ `amount`   decimal NOT NULL ,
+ `entry_id` int NOT NULL ,
+
+PRIMARY KEY (`id`),
+KEY `FK_1` (`entry_id`),
+CONSTRAINT `FK_5` FOREIGN KEY `FK_1` (`entry_id`) REFERENCES `entry` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `visit_history`
+(
+ `id`            int NOT NULL ,
+ `depature_time` datetime NOT NULL ,
+ `payment_id`    int NOT NULL ,
+
+PRIMARY KEY (`id`),
+KEY `FK_1` (`payment_id`),
+CONSTRAINT `FK_4` FOREIGN KEY `FK_1` (`payment_id`) REFERENCES `payment` (`id`)
+);
+
+ALTER TABLE parking_rates
+    ADD CONSTRAINT unique_parking_rate
+    CHECK ((SELECT COUNT(*) FROM parking_rates) = 1); 
