0dfb603e3cb7cf95822a46816d277a3a28864083
Mati-IR <mpabich4@gmail.com>
2024-01-23 14:33:42 +0100

    Backend initial setup

---
+++ backend/server/server.py
@@ -0,0 +1,41 @@
+import httpx
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+#setup logger
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1",
+    # Add other origins if needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, this is parking app serer :)"}
+
+@app.get("/test")
+def test_connection():
+    return {"status": "success", "message": "Connected successfully!"}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
