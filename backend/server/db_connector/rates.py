a512ff80ae53a01f85cf38791fc984eaf4ee61ac
VermiIion <misterpig231@gmail.com>
2024-01-27 00:43:37 +0100

    get_rates_for_client done

--- backend/server/db_connector/rates.py
+++ backend/server/db_connector/rates.py
@@ -1,4 +1,7 @@
 # from fastapi import HTTPException
+from datetime import timedelta
+import math
+from time import strptime
 from .query import query_get, query_put, query_update
 import logging
 from .spots import get_spot_by_reg_number
@@ -42,22 +45,28 @@ def update_rates(hourly_rate, entry_grace_minutes, exit_grace_minutes):
 def get_rates_for_client(reg_number):
     spot = get_spot_by_reg_number(reg_number)
     minutes = __get_minutes_for_client(spot)
+    logger.info(f"minutes{minutes}")
     rates = get_rates()
-    if minutes < rates['entry_grace_minutes']:
-        return 0
+    logger.info(f"rates {rates['entry_grace_minutes']}")
+    if minutes < timedelta(minutes = rates['entry_grace_minutes']):
+     return 0
     else: 
-        return (minutes * rates['hourly_rate'])/60
+     return int(math.ceil(minutes.total_seconds()/60) * rates['hourly_rate'])/60
 
 def __get_minutes_for_client(spot):
     if spot is None:
         return None
     entry_time = spot['entry_time']
-    exit_time = query_get("""
+    temp = query_get("""
             SELECT CURRENT_TIMESTAMP;
                     """,
                       (
                       )
                       )
+    #logger.info(entry_time)
+    #logger.info(temp[0]['CURRENT_TIMESTAMP'])
+    exit_time = temp[0]['CURRENT_TIMESTAMP']
 
-    logger.debug(f"Got minutes for client: entry_time{entry_time}\nexit_time: {exit_time}")
+    logger.info(f"Got minutes for client: entry_time{entry_time}\nexit_time: {exit_time}")
     minutes = exit_time - entry_time
+    return minutes
